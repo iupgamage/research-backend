bda6210569e5875e71c258084d0d1de350d49fcf
IsuruGamage <isuru.gamage@covalentworld.com>
2020-06-20 15:17:38 +0530

    API project added and data creation endpoint added

---
+++ Neo4JSample/Neo4JSample.API/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Neo4JSample.ConsoleApp.Services;
+using Neo4JSample.Settings;
+
+namespace Neo4JSample.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task Post(/*[FromBody] string value*/)
+        {
+            var service = new MovieDataService();
+
+            var settings = ConnectionSettings.CreateBasicAuth("bolt://localhost:7687/db/actors", "neo4j", "test_pwd");
+
+            using (var client = new Neo4JClient(settings))
+            {
+
+                // Create Indices for faster Lookups:
+                //await client.CreateIndices();
+
+                // Create Base Data:
+                await client.CreateMovies(service.Movies);
+                await client.CreatePersons(service.Persons);
+                await client.CreateGenres(service.Genres);
+
+                // Create Relationships:
+                await client.CreateRelationships(service.Metadatas);
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
